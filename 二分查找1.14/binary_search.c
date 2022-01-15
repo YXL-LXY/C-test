ba8ee2f503f8c335f4b22dd52c06da309c35bac4
凯杰 陈 <977859523@qq.com>
2022-01-16 01:16:58 +0800

    .

---
+++ 二分查找1.14/binary_search.c
@@ -0,0 +1,20 @@
+int binary_search(int arr[], int k, int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (k > arr[mid])
+		{
+			left = mid + 1;
+		}
+		else if (k < arr[mid])
+		{
+			right = mid - 1;
+		}
+		else
+			return mid;
+	}
+	return -1;
+}
---
+++ 二分查找1.14/binary_search.h
@@ -0,0 +1,6 @@
+#ifndef __binary_search_h__
+#define __binary_search_h__
+
+int binary_search(int arr[], int k, int sz);
+
+#endif
