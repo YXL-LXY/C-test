b30e4ab39ea066efc92989127de66e3e6876079d
凯杰 陈 <977859523@qq.com>
2022-02-11 22:29:45 +0800

    .

---
+++ test.2.11/test.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+
+//void reverse(char* str)
+//{
+//	int len = strlen(str);
+//	char* left = str;
+//	char* right = str + len - 1;
+//	while (left<right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//int main()
+//{
+//	char arr[256] = { 0 };
+//	gets(arr);
+//	reverse(arr);//ÄæÐòº¯Êý
+//	printf("%s",arr);
+//	return 0;
+//}
+
+
+
+
