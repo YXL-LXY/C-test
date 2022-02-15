a178509d9f02553d3da79b2e36f067425e9be812
凯杰 陈 <977859523@qq.com>
2022-02-15 23:10:38 +0800

    .

--- test.2.11/test.c
+++ test.2.11/test.c
@@ -109,39 +109,94 @@
 //反转字符串
 
 //三步反转法
+//
+//void reverse(char* left,char* right)
+//{
+//	assert(left);
+//	assert(right);
+//	while (left < right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//void left_move(char* arr, int k)
+//{
+//	assert(arr);
+//	int len = strlen(arr);
+//	assert(k <= len);
+//	reverse(arr,arr+k-1);//逆序左边
+//	reverse(arr+k,arr+len-1);//逆序右边
+//	reverse(arr,arr+len-1);//逆序整体
+//
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	char arr[] = "abcdef";
+//	printf("请输入反转字符个数：");
+//	scanf("%d",&n);
+//	left_move(arr, n);
+//	printf("%s\n", arr);
+//	return 0;
+//}
 
-void reverse(char* left,char* right)
+
+
+
+
+//杨氏矩阵
+//
+// 有一个数字矩阵，矩阵的每行从左到右递增，从上到下递增，请编写程序在这样的矩阵中查找某个数字是否存在
+// 
+// 要求：时间复杂度小于O(N);
+//
+
+int FindNum(int arr[3][3], int k,int *px,int *py)//比较矩阵右上角数据和要找数据的大小，去掉一行或一列
 {
-	assert(left);
-	assert(right);
-	while (left < right)
+	int a = 0;
+	int b = *py - 1;
+	while (a <= *px - 1 && b >= 0)
 	{
-		char tmp = *left;
-		*left = *right;
-		*right = tmp;
-		left++;
-		right--;
+		if (arr[a][b] > k)
+		{
+			b--;//去掉最右边一列
+		}
+		else if (arr[a][b] < k)
+		{
+			a++;//去掉第一行
+		}
+		else
+		{
+			*px = a;
+			*py = b;
+			return 1;
+		}
 	}
-}
-
-void left_move(char* arr, int k)
-{
-	assert(arr);
-	int len = strlen(arr);
-	assert(k <= len);
-	reverse(arr,arr+k-1);//逆序左边
-	reverse(arr+k,arr+len-1);//逆序右边
-	reverse(arr,arr+len-1);//逆序整体
-
+	//找不到
+	return 0;
 }
 
 int main()
 {
-	int n = 0;
-	char arr[] = "abcdef";
-	printf("请输入反转字符个数：");
-	scanf("%d",&n);
-	left_move(arr, n);
-	printf("%s\n", arr);
+	int arr[3][3] = { 1,2,3,4,5,6,7,8,9 };
+	int k = 17;
+	int x = 3;
+	int y = 3;
+	int ret = FindNum(arr, k, &x, &y);
+	if (ret == 1)
+	{
+		printf("找到了\n");
+		printf("下标是：%d %d",x,y);
+	}
+	else
+	{
+		printf("找不到");
+	}
 	return 0;
 }
