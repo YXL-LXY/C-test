eea20fada6b998d47182a0953fb7727977b461c8
凯杰 陈 <977859523@qq.com>
2022-02-18 14:43:01 +0800

    .

--- 模拟实现库函数/test.c
+++ 模拟实现库函数/test.c
@@ -1,4 +1,4 @@
-
+#define _CRT_SECUURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<string.h>
 #include<assert.h>
@@ -33,27 +33,80 @@
 //strcat
 
 
-char* my_strcat(char* dest, const char* src)
+//char* my_strcat(char* dest, const char* src)
+//{
+//	char* ret = dest;
+//	//找到目的字符串的\0
+//	while (*dest != '\0')
+//	{
+//		dest++;
+//	}
+//	//追加
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	char arr1[20] = "hello";
+//	char arr2[] = "world";
+//	my_strcat(arr1, arr2);
+//	printf("%s\n",arr1);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+//实现strlen的三种方法
+//1
+//int My_strlen1(char* x)
+//{
+//	int count = 0;
+//	while (*x)
+//	{
+//		count++;
+//		x++;
+//	}
+//	return count;
+//}
+//
+
+
+
+//2
+//int My_strlen2(char* x)
+//{
+//	if (*x)
+//		return 0;
+//	else
+//		return 1 + My_strlen2(x + 1);
+//}
+
+
+//3 指针减指针
+int My_strlen3(char* x)
 {
-	char* ret = dest;
-	//找到目的字符串的\0
-	while (*dest != '\0')
+	char* q = x;
+	while (*x)
 	{
-		dest++;
+		x++;
 	}
-	//追加
-	while (*dest++ = *src++)
-	{
-		;
-	}
-	return ret;
+	return x - q;
 }
+
 
 int main()
 {
-	char arr1[20] = "hello";
-	char arr2[] = "world";
-	my_strcat(arr1, arr2);
-	printf("%s\n",arr1);
+	char arr[] = "HELLO WORLD!";
+	int ret = My_strlen3(arr);
+	printf("%d \n", ret);
 	return 0;
 }
